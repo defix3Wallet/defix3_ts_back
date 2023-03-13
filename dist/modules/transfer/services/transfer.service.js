2c8aa127583779ec090b08d9cf19db298edc8cc6
Juan Ochando <juanochando00@gmail.com>
2023-03-13 15:41:50 -0400

    oficina

--- dist/modules/transfer/services/transfer.service.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TransferService = void 0;
-class TransferService {
-}
-exports.TransferService = TransferService;
